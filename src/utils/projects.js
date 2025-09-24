a1e932c2323eb2815e4666ae72692ea9100a3c91
andriy-shtumpf <shtumpf@gmail.com>
2025-09-24 00:50:37 -0400

    fixing project link

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -3,7 +3,7 @@ const works = [
         imgSrc: "/images/events-board.png",
         title: "Events-board",
         tags: ["Next js", "Prisma", "Postgres", "AWS Amplify", "Stripe"],
-        projectLink: "https://master.d16jh2qsui6dp9.amplifyapp.com",
+        projectLink: "https://develop.d16jh2qsui6dp9.amplifyapp.com",
         githubLink: "https://github.com/portfoglio/events-board",
     },
     {
@@ -20,28 +20,6 @@ const works = [
         projectLink: "",
         githubLink: "",
     },
-    // {
-    //     imgSrc: "/images/project-4.jpg",
-    //     title: "Real state website",
-    //     tags: ["Web-design", "Development"],
-    //     projectLink: "https://github.com/codewithsadee-org/wealthome",
-    //     githubLink: "https://github.com/portfoglio/events-board",
-    // },
-    // {
-    //     imgSrc: "/images/project-5.jpg",
-    //     title: "eCommerce website",
-    //     tags: ["eCommerce", "Development"],
-    //     projectLink: "https://github.com/codewithsadee/anon-ecommerce-website",
-    //     githubLink: "https://github.com/portfoglio/events-board",
-    // },
-    // {
-    //     imgSrc: "/images/project-6.jpg",
-    //     title: "vCard Personal portfolio",
-    //     tags: ["Web-design", "Development"],
-    //     projectLink:
-    //         "https://github.com/codewithsadee/vcard-personal-portfolio",
-    //     githubLink: "https://github.com/portfoglio/events-board",
-    // },
 ];
 
 export { works };
